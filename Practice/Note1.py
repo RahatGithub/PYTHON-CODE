39c81c61ca62c032a5def4c01c8c55d285fcdc7f
rahatahmedchowdhuryrac@gmail.com <rahatahmedchowdhuryrac@gmail.com>
2021-09-25 16:28:30 +0600

    Mutation.py added

--- Practice/Note1.py
+++ Practice/Note1.py
@@ -1,13 +1,26 @@
-"""পাইথনে কোনো মিউটেবল ভ্যারিয়েবলের মধ্যে অন্য ভ্যারিয়েবল এসাইন করলে 
-শুধুমাত্র ঐ ভ্যারিয়েবলের মানই এসাইন হয় না বরং তারা কলাবরেট করে 
-ফেলে। এর পর থেকে ১ম ভ্যারিয়েবলের উপর করা অপারেশনটি 
-আপনা-আপনিই ২য় ভ্যারিয়েবলের উপরও সংঘটিত হয়ে যায়।""" 
+"""In Python, when we assign one variable to another, then if we do 
+any operation on the first one then it automatically applies
+on the second one too. e.g. when we have assigned 'li' to a 
+new variable 'new_li', then these two lists colaborate with 
+each other. So, sorting 'li' automatically sorted 'new_li'.""" 
 
 
 li = [4, 7, 6, 1, 3, 5]
 
-new_li = li #নতুন লিস্টে আগের লিস্টটি এসাইন হয়েছে
+new_li = li 
 
-li.sort() #আগের লিস্টটিকে সর্ট করা হয়েছে
+li.sort() #Sorted the old list
 
-print(li, new_li) #উভয় লিস্টই সর্ট হয়ে গেছে
+#Print 'li' and 'new_li' to see that both lists are sorted
+
+
+"""To avoid this, we need to assign the old list saying that it 
+is a list. like below- """
+
+li = [4, 7, 6, 1, 3, 5]
+
+new_li = list(li)
+
+li.sort() #Sorted the old list
+
+#Print 'li' and 'new_li' to see that only 'li' is sorted
