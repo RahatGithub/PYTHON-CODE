0f7ed701e125616be999cf23b43d1f135068debd
rahatahmedchowdhuryrac@gmail.com <rahatahmedchowdhuryrac@gmail.com>
2021-10-26 08:53:10 +0600

    SParse Arrays added

--- ProblemSolving/DS/test.py
+++ ProblemSolving/DS/test.py
@@ -1,11 +1,5 @@
-# a1 = [2,4,5,7,8,1,3,0]
+s = ['abc', 'ab', 'ab', 'bbc', 'ab']
 
-# a2 = a1[:3] + a1[7:2:-1] 
+res = s.count('ab')
 
-# print(a1, a2)
-
-arr = [1, 2, 3, 4, 5]
-
-arr = arr[2:] + arr[:2]
-
-print(arr)
+print(res)
