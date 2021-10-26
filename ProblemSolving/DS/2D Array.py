7e65af09272749cba66791d4e6cf2059394deb25
rahatahmedchowdhuryrac@gmail.com <rahatahmedchowdhuryrac@gmail.com>
2021-10-26 07:56:01 +0600

    Dynamic Array added

--- ProblemSolving/DS/2D Array.py
+++ ProblemSolving/DS/2D Array.py
@@ -1,20 +1,20 @@
 def hourglassSum(arr):
     
-    maxSum = -100
+    maxSum = -100                                           #let maxSum be anything smaller then -63 [-9*7 = -63]
 
-    for row in range(4):
+    for row in range(4):                                    #Don't need to check all 36(6X6) elements; just checking 16(4X4) elements
 
         for col in range(4):
 
-            top = sum(arr[row][col:col+3])
+            top = sum(arr[row][col:col+3])                  #Storing the sum of top column in 'top'
 
-            mid = arr[row+1][col+1]
+            mid = arr[row+1][col+1]                         #Storing the mid element in 'mid'
 
-            bottom = sum(arr[row+2][col:col+3])
+            bottom = sum(arr[row+2][col:col+3])             #Storing the sum of bottom column in 'bottom'
 
             hourglass = top + mid + bottom
 
-            maxSum = max(maxSum, hourglass)
+            maxSum = max(maxSum, hourglass)                 #If maxSum is already greater then hourglass then fine, otherwise reassign maxSum as hourglass
             
     return maxSum
     
@@ -22,6 +22,6 @@ def hourglassSum(arr):
 arr = []
 
 for _ in range(6):
-    arr.append(list(map(int, input().rstrip().split())))
+    arr.append(list(map(int, input().rstrip().split())))    #Taking input of 2D array
 
 print(hourglassSum(arr))
