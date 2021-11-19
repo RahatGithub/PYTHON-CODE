81452a1f24a8907766b8dc9018c8c2aeda62da8b
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2021-11-19 12:49:58 +0600

    BitwiseOperators added

---
+++ Practice/BitwiseOperators.py
@@ -0,0 +1,19 @@
+"""Bitwise operators
+
+   COMPLEMENT (~)
+   LEFT SHIFT (<<)
+   RIGHT SHIFT (>>)
+   AND (&)
+   OR (|)
+   XOR (^)"""
+   
+
+# Complement
+~4 # output: -5; Formula: ~n = - n - 1
+
+# Left shift
+10 << 4 # output: 160; Formula: m << n = m*(2**n) [multiplies with the power of 2]
+
+# Right shift
+40 << 3 # output: 5; Formula: m >> n = m/(2**n) [devides by the power of 2]
+
--- Practice/test.py
+++ Practice/test.py
@@ -1,17 +1,19 @@
-t = int(input())
+# =============================================================================
+# def isPowerOf2(num):
+#     
+#     check = not(num & (num-1))  //if 0 then num can be expressed as power of smthing else 'bhag ekhan theke'
+#     
+#     // only not(0) == True; otherwise for any n, not(n) == False
+#     
+#     return check
+# =============================================================================
 
-for _ in range(t):
+
+def isPowerOf2(num):
     
-    keyboard, string, time, current_position = input(), input(), 0, 0
-    
-    for i in range(0, len(string)):
-        
-        new_position = keyboard.index(string[i]) 
-        
-        if not i==0:
-            
-            time += abs(new_position - current_position)
-            
-        current_position = new_position
-    
-    print(time)
+    return not(num & (num-1))
+
+
+n = int(input())
+
+print(isPowerOf2(n))
