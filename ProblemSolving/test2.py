99f4bb4543fd985c6a20e15e70557e4a3ac3c7b6
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2021-12-01 19:16:24 +0600

    Game of two stacks tried

---
+++ ProblemSolving/test2.py
@@ -0,0 +1,59 @@
+def twoStacks(a, b, maxSum):
+
+    total_removed = count = 0
+    
+    while not (len(a) == len(b) == 0) : 
+        
+        # if a and b:           
+        #     total_removed += a.pop() if min(a[-1],b[-1]) == a[-1] else b.pop() 
+        #     if total_removed <= maxSum : count += 1 
+        #     else : return count 
+            
+
+        if (a and b) and min(a[-1],b[-1]) == a[-1] :       
+            total_removed += a.pop()
+            if total_removed <= maxSum : count += 1
+            else : return count 
+        
+        if (a and b) and min(a[-1],b[-1]) == b[-1] :           
+            total_removed += b.pop()
+            if total_removed <= maxSum : count += 1
+            else : return count 
+        ##########################################
+        
+        if a and not b :
+            total_removed += a.pop()
+            if total_removed <= maxSum : count += 1
+            else : return count 
+        
+        if b and not a :
+            total_removed += b.pop()
+            if total_removed <= maxSum : count += 1
+            else : return count 
+            
+    return count
+        
+
+for _ in range(int(input())):
+
+    n, m, maxSum = map(int, input().split())
+    
+    a = list(map(int, input().split()))[::-1]
+    
+    b = list(map(int, input().split()))[::-1]
+    
+    print (twoStacks(a, b, maxSum)) 
+    
+    
+            
+        ############################################
+        # if (a and b) and min(a[-1],b[-1]) == a[-1] :       
+        #     total_removed += a.pop()
+        #     if total_removed <= maxSum : count += 1
+        #     else : return count 
+        
+        # if (a and b) and min(a[-1],b[-1]) == b[-1] :           
+        #     total_removed += b.pop()
+        #     if total_removed <= maxSum : count += 1
+        #     else : return count 
+        # ##########################################
