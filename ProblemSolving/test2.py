039524515f0358929f98c5bb10de3bf73e6e2eba
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2021-12-06 13:22:09 +0600

    2D Arrays-HR added

--- ProblemSolving/test2.py
+++ ProblemSolving/test2.py
@@ -1,59 +1,9 @@
-def twoStacks(a, b, maxSum):
+def isSingle(nums) : 
+    z = 0
+    for num in nums : 
+        z = z ^ num   
+    return z
 
-    total_removed = count = 0
-    
-    while not (len(a) == len(b) == 0) : 
-        
-        # if a and b:           
-        #     total_removed += a.pop() if min(a[-1],b[-1]) == a[-1] else b.pop() 
-        #     if total_removed <= maxSum : count += 1 
-        #     else : return count 
-            
+nums = list(map(int, input().split()))
 
-        if (a and b) and min(a[-1],b[-1]) == a[-1] :       
-            total_removed += a.pop()
-            if total_removed <= maxSum : count += 1
-            else : return count 
-        
-        if (a and b) and min(a[-1],b[-1]) == b[-1] :           
-            total_removed += b.pop()
-            if total_removed <= maxSum : count += 1
-            else : return count 
-        ##########################################
-        
-        if a and not b :
-            total_removed += a.pop()
-            if total_removed <= maxSum : count += 1
-            else : return count 
-        
-        if b and not a :
-            total_removed += b.pop()
-            if total_removed <= maxSum : count += 1
-            else : return count 
-            
-    return count
-        
-
-for _ in range(int(input())):
-
-    n, m, maxSum = map(int, input().split())
-    
-    a = list(map(int, input().split()))[::-1]
-    
-    b = list(map(int, input().split()))[::-1]
-    
-    print (twoStacks(a, b, maxSum)) 
-    
-    
-            
-        ############################################
-        # if (a and b) and min(a[-1],b[-1]) == a[-1] :       
-        #     total_removed += a.pop()
-        #     if total_removed <= maxSum : count += 1
-        #     else : return count 
-        
-        # if (a and b) and min(a[-1],b[-1]) == b[-1] :           
-        #     total_removed += b.pop()
-        #     if total_removed <= maxSum : count += 1
-        #     else : return count 
-        # ##########################################
+print(isSingle(nums))
