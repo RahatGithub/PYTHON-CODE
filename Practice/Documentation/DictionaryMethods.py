8698c6ff4eb3fa867b096d9573b2313ecec2ea3c
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-04-11 02:07:21 +0600

    dict.fromkeys() added

--- Practice/Documentation/DictionaryMethods.py
+++ Practice/Documentation/DictionaryMethods.py
@@ -24,3 +24,16 @@ favFruits.popitem()
 # Pops an element with a specific key. In this case, the key is 'Sam' 
 favFruits.pop('Sam')
 
+
+# copies the 'favFruits' dictionary into 'reservedFruits' dictionary. As dictionaries are mutable objects, 
+# we need to copy the dictionary into another value and then do operations on them
+reservedFruits = favFruits.copy()
+
+
+# returns a dictionary with specific keys and a same value. If the value is not given then 'None' is assigned automatically
+students = ('Adam', 'Saleh', 'Billah', 'Ramanov', 'Joseph')
+dept = 'EEE'
+studentsInfo = dict.fromkeys(students, dept)  # sudentsInfo dictionary have 5 keys named, Adam, Saleh, Billah, Ramanov and Joseph. Value of all of them is 'EEE'
+
+
+# 
