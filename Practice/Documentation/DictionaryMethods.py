4d81f2e2919750932eef412fa138041a7d8ec070
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-04-11 12:08:07 +0600

    Dictionary methods done

--- Practice/Documentation/DictionaryMethods.py
+++ Practice/Documentation/DictionaryMethods.py
@@ -36,4 +36,16 @@ dept = 'EEE'
 studentsInfo = dict.fromkeys(students, dept)  # sudentsInfo dictionary have 5 keys named, Adam, Saleh, Billah, Ramanov and Joseph. Value of all of them is 'EEE'
 
 
-# 
+# Returns the value of the specified key. If the key does not exist: insert the key, with the specified value.
+# Here, the value of 'man_of_the_match_score' is 154 as 'Liton' is not in the dictionary. If it was 'Mushfiq' 
+# then the value of 'man_of_the_match_score' would be 67
+scores = {'Tamim':22, 'Shakib':58, 'Mushfiq':67, 'Mahmudullah':19}
+man_of_the_match_score = scores.setdefault('Liton', 154)
+
+
+# Deletes a item from the dictionary
+del scores['Tamim']
+
+
+# Clears all the items of the dictionary
+scores.clear()
