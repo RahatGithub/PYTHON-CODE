69e0dac6a8fd5936b33a7328a5613c95ff2789d6
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-06-04 09:49:29 +0600

    Enumerate added

--- Practice/General/general.py
+++ Practice/General/general.py
@@ -1,3 +1,17 @@
-info = {"Jamil":"Sylhet", "Hammad":"Khulna", "Rouf":"Dhaka"}
+import time
 
-print(len(info))
+init1 = time.time()
+x = 0
+for i in range(1, 100001): x+=1
+time.sleep(1)
+final1 = time.time()
+print(f"Total time for FOR loop: {final1-init1}")
+
+init2 = time.time()
+x = 0
+i = 0
+while i < 100000:
+    x+=1
+    i+=1
+final2 = time.time()
+print(f"Total time for WHILE loop: {final2-init2}")
