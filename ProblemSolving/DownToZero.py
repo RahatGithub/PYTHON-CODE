d54312b3474da19985043035101ae3bcbd79abca
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2024-01-03 10:16:20 +0600

    Yo practice

---
+++ ProblemSolving/DownToZero.py
@@ -0,0 +1,13 @@
+def down_to_zero:
+    pass 
+
+
+if __name__ == "__main__":
+    Q = int(input())
+    N_list = []
+    for _ in range(Q):
+        N = int(input())
+        N_list.append(N)
+    
+    print(N_list)
+    
